31e284c0736313915869c304c8b53bb4e1926f81
Adolphe <naphe12@yahoo.fr>
2025-12-07 15:29:18 +0100

    debug7 commit

--- app/routers/debug.py
+++ app/routers/debug.py
@@ -1,30 +1,27 @@
 import httpx
-from fastapi import FastAPI
+from fastapi import APIRouter
+
 from app.core.config import settings
-app = FastAPI()
 
-BREVO_API_KEY = settings.BREVO_API_KEY
+router = APIRouter(tags=["Debug"])
 
-@app.get("/send-email")
+
+@router.get("/send-email")
 async def send_email():
     payload = {
         "sender": {"email": "adolphe.nahimana@gmail.com", "name": "PayLink App"},
         "to": [{"email": "naphe12@yahoo.fr"}],
         "subject": "Test Brevo",
-        "htmlContent": "<h1>Email envoyé via Brevo 🎉</h1>"
+        "htmlContent": "<h1>Email envoyé¸ via Brevo ÐYZ%</h1>",
     }
 
-    headers = {
-        "api-key": BREVO_API_KEY,
-        "Content-Type": "application/json"
-    }
+    headers = {"api-key": settings.BREVO_API_KEY, "Content-Type": "application/json"}
 
     async with httpx.AsyncClient() as client:
         response = await client.post(
             "https://api.brevo.com/v3/smtp/email",
             json=payload,
-            headers=headers
+            headers=headers,
         )
-        return response.json()
 
-
+    return response.json()
